eb4ff3f81361f13c079a9698fe430097871eaa53
Sudarsan <sudmnr11@gmail.com>
2026-03-11 22:26:09 -0400

    return json response

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,14 +1,14 @@
-
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.model.HelloResponse;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello world, spring is here";
+    public HelloResponse hello() {
+        return new HelloResponse("Hello world, spring is here");
     }
 }
---
+++ src/main/java/com/example/model/HelloResponse.java
@@ -0,0 +1,28 @@
+
+package com.example.model;
+
+public class HelloResponse {
+    private String message;
+    private long timestamp;
+
+    public HelloResponse(String message) {
+        this.message = message;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
